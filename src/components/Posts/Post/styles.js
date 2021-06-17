343337841673867c4b2d04722185184755905840
Chuck3232 <61963488+Chuck3232@users.noreply.github.com>
2021-06-17 14:34:17 +0200

    css fix

--- src/components/Posts/Post/styles.js
+++ src/components/Posts/Post/styles.js
@@ -39,8 +39,7 @@ export default makeStyles({
     borderRadius: 3,
     boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     color: 'white',
-    padding: '0 30px',
-    size="small"
+    size: "small"
   },
   grid: {
     display: 'flex',
